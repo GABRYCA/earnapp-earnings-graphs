779f4dddd096cf1a0c8d41075f2ae53625d02be1
AnonymousGCA <gabrycaretti@gmail.com>
2021-12-19 17:40:42 +0100

    Changelogs: - Homepage. - Contacts. - Referral button. - Header. - Footer.

---
+++ js/footer.js
@@ -0,0 +1,19 @@
+document.getElementById("footer").innerHTML = "<hr style='color: #dedede'><div class='container-fluid' id='footer'>" +
+    "<hr>" +
+    "<div class='container'>" +
+    "<div class='row text-start'>" +
+    "<div class='col'>" +
+    "<p class='h5 pb-1'>Source code:</p>" +
+    "<p class='h6'><a href='https://github.com/GABRYCA/earnapp-earnings-graphs' style='color: white; text-decoration: none'>Github</a></p>" +
+    "</div>" +
+    "<div class='col'>" +
+    "<p class='h5 pb-1'>Contacts:</p>" +
+    "<p class='h6'><a href='https://discord.gg/BGTB8XCfZK' style='color: white; text-decoration: none'>Discord</a></p>" +
+    "</div>" +
+    "<div class='col'>" +
+    "<p class='h5'>Made in EU (Region: Italy).</p>" +
+    "<p class='h6'>By AnonymousGCA (aka GABRYCA).</p>" +
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    "</div>"
---
+++ js/header.js
@@ -0,0 +1,14 @@
+document.getElementById("header").innerHTML = "<p class='h1 pt-2 pb-2'>AnonymousGCA Earnapp Graphs</p>" +
+    "<div class='d-flex justify-content-between bg-dark bg-opacity-50 rounded-3 border border-success'>" +
+    "<ul class='nav m-auto bg-black bg-opacity-25'>" +
+    "<li class='nav-item border-start border-success rounded-3'>" +
+    "<a class='nav-link' href='home.html'>Home</a>" +
+    "</li>" +
+    "<li class='nav-item border-start border-end border-success'>" +
+    "<a class='nav-link' href='https://github.com/GABRYCA'>Github</a>" +
+    "</li>" +
+    "<li class='nav-item border-end border-success rounded-3'>" +
+    "<a class='nav-link' href='contacts.html'>Contacts</a>" +
+    "</li>" +
+    "</ul>" +
+    "</div>"

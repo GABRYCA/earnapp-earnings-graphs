46c75940f63ccf3e68dd8454fa9414ce5817efdd
AnonymousGCA <gabrycaretti@gmail.com>
2021-12-20 21:41:57 +0100

    Changelogs: - Better footer on mobile.

--- js/footer.js
+++ js/footer.js
@@ -2,16 +2,14 @@ document.getElementById("footer").innerHTML = "<hr style='color: #dedede'><div c
     "<hr>" +
     "<div class='container'>" +
     "<div class='row text-start'>" +
-    "<div class='col-sm pt-3'>" +
+    "<div class='col-sm pt-3 border-bottom border-light'>" +
     "<p class='h5 pb-1'>Source code:</p>" +
     "<p class='h6'><a href='https://github.com/GABRYCA/earnapp-earnings-graphs' style='color: white; text-decoration: none'>Github</a></p>" +
     "</div>" +
-    "<hr style='color: #dedede'>" +
-    "<div class='col-sm pt-3'>" +
+    "<div class='col-sm pt-3 border-bottom border-light'>" +
     "<p class='h5 pb-1'>Contacts:</p>" +
     "<p class='h6'><a href='https://discord.gg/BGTB8XCfZK' style='color: white; text-decoration: none'>Discord</a></p>" +
     "</div>" +
-    "<hr style='color: #dedede'>" +
     "<div class='col-sm pt-3'>" +
     "<p class='h5'>Made in EU (Region: Italy).</p>" +
     "<p class='h6'>By AnonymousGCA (aka GABRYCA).</p>" +

65206c5f748cba7837ae2c58504702a33c8c0084
AnonymousGCA <gabrycaretti@gmail.com>
2021-12-20 19:09:22 +0100

    Changelogs: - Better footer for mobile. - Better homepage on mobile.
    TODO:
    - Enable/configure all php pages.

--- js/footer.js
+++ js/footer.js
@@ -2,15 +2,17 @@ document.getElementById("footer").innerHTML = "<hr style='color: #dedede'><div c
     "<hr>" +
     "<div class='container'>" +
     "<div class='row text-start'>" +
-    "<div class='col'>" +
+    "<div class='col-sm pt-3'>" +
     "<p class='h5 pb-1'>Source code:</p>" +
     "<p class='h6'><a href='https://github.com/GABRYCA/earnapp-earnings-graphs' style='color: white; text-decoration: none'>Github</a></p>" +
     "</div>" +
-    "<div class='col'>" +
+    "<hr style='color: #dedede'>" +
+    "<div class='col-sm pt-3'>" +
     "<p class='h5 pb-1'>Contacts:</p>" +
     "<p class='h6'><a href='https://discord.gg/BGTB8XCfZK' style='color: white; text-decoration: none'>Discord</a></p>" +
     "</div>" +
-    "<div class='col'>" +
+    "<hr style='color: #dedede'>" +
+    "<div class='col-sm pt-3'>" +
     "<p class='h5'>Made in EU (Region: Italy).</p>" +
     "<p class='h6'>By AnonymousGCA (aka GABRYCA).</p>" +
     "</div>" +

68cb7831853886a2909abcde4f8966ad0f6e0b91
AnonymousGCA <gabrycaretti@gmail.com>
2022-01-08 14:51:21 +0100

    Changelogs: - Added Daily earnings and traffic Graphs.

--- graphs.php
+++ graphs.php
@@ -14,15 +14,32 @@ if ($conn->connect_error) {
 
 $dataPointsEarnings = array();
 $dataPointsTraffic = array();
+$dataPointsEarningsDaily = array();
+$dataPointsTrafficDaily = array();
 $result = mysqli_query($conn, "SELECT * FROM earnings");
 
+$counter = 0;
+$sumEarnings = 0;
+$sumTraffic = 0;
 while($row = mysqli_fetch_array($result))
 {
     $phpDate = date($row['time']);
     $phpTimestamp = strtotime($phpDate);
     $javaScriptTimestamp = $phpTimestamp * 1000;
-    $earned = array("x" =>  $javaScriptTimestamp, "y" => (float) $row['earnings']);
-    $traffic = array("x" =>  $javaScriptTimestamp, "y" => (float) $row['traffic']);
+    $earningsData = $row['earnings'];
+    $trafficData = $row['traffic'];
+    $earned = array("x" =>  $javaScriptTimestamp, "y" => (float) $earningsData);
+    $traffic = array("x" =>  $javaScriptTimestamp, "y" => (float) $trafficData);
+    $sumEarnings += (float) $earningsData;
+    $sumTraffic += (float) $trafficData;
+    $counter++;
+
+    if ($counter % 24 === 0){
+        $dataPointsEarningsDaily[] = array("x" => $javaScriptTimestamp, "y" => (float) $sumEarnings);
+        $dataPointsTrafficDaily[] = array("x" => $javaScriptTimestamp, "y" => (float) $sumTraffic);
+        $sumEarnings = 0;
+        $sumTraffic = 0;
+    }
 
     $dataPointsEarnings[] = $earned;
     $dataPointsTraffic[] = $traffic;
@@ -94,11 +111,62 @@ $conn->close();
                 }]
             });
 
+            var chart3 = new CanvasJS.Chart("chartEarningsDaily", {
+                animationEnabled: true,
+                zoomEnabled: true,
+                theme: "dark2",
+                title:{
+                    text: "Daily Earnings",
+                },
+                axisX: {
+                    valueFormatString: "DD-MM-YY",
+                },
+                axisY: {
+                    title: "Cents Earned",
+                    includeZero: true,
+                    valueFormatString: "$##0.00",
+                },
+                data: [{
+                    type: "splineArea",
+                    color: "#21ab40",
+                    xValueType: "dateTime",
+                    xValueFormatString: "DDD",
+                    yValueFormatString: "$##0.00",
+                    dataPoints: <?php echo json_encode($dataPointsEarningsDaily); ?>
+                }]
+            });
+
+            var chart4 = new CanvasJS.Chart("chartTrafficDaily", {
+                animationEnabled: true,
+                zoomEnabled: true,
+                theme: "dark2",
+                title:{
+                    text: "Daily Traffic",
+                },
+                axisX: {
+                    valueFormatString: "DD-MM-YY",
+                },
+                axisY: {
+                    title: "Traffic MegaBytes",
+                    includeZero: true,
+                    valueFormatString: "##0.00MB",
+                },
+                data: [{
+                    type: "splineArea",
+                    color: "#3989ef",
+                    xValueType: "dateTime",
+                    xValueFormatString: "DDD",
+                    yValueFormatString: "##0.00MB",
+                    dataPoints: <?php echo json_encode($dataPointsTrafficDaily); ?>
+                }]
+            });
+
             currentUser();
 
             chart.render();
             chart2.render();
-
+            chart3.render();
+            chart4.render();
         }
         function currentUser(){
             document.getElementById("user").innerText = "Current user: " + "<?php echo $_POST["showname"]?>"
@@ -119,6 +187,10 @@ $conn->close();
         <div id="chartEarnings" style="height: 370px; width: 100%;"></div>
         <br>
         <div id="chartTraffic" style="height: 370px; width: 100%;"></div>
+        <br>
+        <div id="chartEarningsDaily" style="height: 370px; width: 100%;"></div>
+        <br>
+        <div id="chartTrafficDaily" style="height: 370px; width: 100%;"></div>
     </div>
 
     <div class="pt-5" id="footer"></div>

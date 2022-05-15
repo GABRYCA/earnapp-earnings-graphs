5f65ac0bda77d70aa833f7eddc89b4e0595aa6e9
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2022-05-15 23:57:49 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ document.getElementById("header").innerHTML = "<p class='h1 pt-2 pb-2'>Anonymous
     "<div class='d-flex justify-content-between bg-dark bg-opacity-50 rounded-3 border border-success'>" +
     "<ul class='nav m-auto bg-black bg-opacity-25'>" +
     "<li class='nav-item border-start border-success rounded-3'>" +
-    "<a class='nav-link' href='home.html'>Home</a>" +
+    "<a class='nav-link' href='/'>Home</a>" +
     "</li>" +
     "<li class='nav-item border-start border-end border-success'>" +
     "<a class='nav-link' href='https://github.com/GABRYCA' target='_blank'>Github</a>" +
@@ -10,8 +10,11 @@ document.getElementById("header").innerHTML = "<p class='h1 pt-2 pb-2'>Anonymous
     "<li class='nav-item border-start border-end border-success'>" +
     "<a class='nav-link' href='https://earnapp.com/i/6fnma8w' target='_blank'>Referral</a>" +
     "</li>" +
-    "<li class='nav-item border-end border-success rounded-3'>" +
+    "<li class='nav-item border-start border-end border-success rounded-3'>" +
     "<a class='nav-link' href='contacts.html'>Contacts</a>" +
+    "</li>" +
+    "<li class='nav-item border-end border-success rounded-3'>" +
+    "<a class='nav-link' href='https://anonymousgca.eu/passive_income.html'>Passive Income</a>" +
     "</li>" +
     "</ul>" +
     "</div>"
